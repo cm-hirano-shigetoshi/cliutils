6c9a725761b5e11bffbbe81dff1f64390f68aba4
ShigetoshiHirano <hirano.shigetoshi@classmethod.jp>
2018-09-11 22:22:12 +0900

    いくつかの基本的な動作を実装

--- src/vemu.nim
+++ src/vemu.nim
@@ -23,18 +23,43 @@ except:
   quit(1)
 
 
-let cursor = 0
+#Tue Sep 11 21:21:18 DST 2018
+#0123456789012345678901234567
+var cursor = 0
 
-proc findRange(): int =
-  return 4
+
+proc findEnd(line: string): int =
+  var s: int
+  var t: int
+  s = line.find(" ", cursor+2)
+  t = line.find("\t", cursor+2)
+  if s == -1 and t == -1:
+    return -1
+  elif s == -1:
+    return t - 1
+  elif t == -1:
+    return s - 1
+  else:
+    return min(s,t) - 1
+
+proc findW(line: string): int =
+  var i = findEnd(line)
+  if i == -1:
+    return i
+  i += 1
+  while $line[i] == " " or $line[i] == "\t":
+    i += 1
+  return i
 
 proc operate(line: string, action: string, pos: string): string =
   var target = -1
   if pos == "END":
-    target = findRange()
+    target = findEnd(line)
   if action == "delete":
-    echo "line[0..", cursor-1, "] & line[", cursor+target+1, "..", line.len-1, "]"
-    return line[0 .. cursor-1] & line[cursor+target+1 .. line.len-1]
+    if target >= 0:
+      return line[0 .. cursor-1] & line[target+1 .. line.len-1]
+    else:
+      return line[0 .. cursor-1]
 
 let query = args[0]; args.delete(0)
 
@@ -43,13 +68,43 @@ for orig_line in readLinesFromFileOrStdin(args):
   var line = orig_line
   var action = ""
   var pos = ""
+  cursor = 0
   while query_index < query.len:
     let q = $query[query_index]
     if action == "":
+      if q == "D":
+        line = line[0 .. cursor-1]
+        query_index += 1
+        continue
       if q == "d":
         action = "delete"
         query_index += 1
         continue
+      if q == "f":
+        action = "find"
+        query_index += 1
+        continue
+      if q == "h":
+        cursor = max(0, cursor-1)
+        query_index += 1
+        continue
+      if q == "l":
+        cursor += 1
+        query_index += 1
+        continue
+      if q == "W":
+        cursor = findW(line)
+        query_index += 1
+        continue
+      if q == "E":
+        cursor = findEnd(line)
+        query_index += 1
+        continue
+    elif action == "find":
+      cursor = line.find(q, cursor)
+      action = ""
+      query_index += 1
+      continue
     else:
       if q == "E":
         pos="END"
@@ -58,6 +113,7 @@ for orig_line in readLinesFromFileOrStdin(args):
         pos = ""
         query_index += 1
         echo line
-        break
-        #continue
+        continue
+    stderr.write "Unsupported char: ", q
+  echo line
 
